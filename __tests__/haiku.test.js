afca46202f5a6582e4fcd7f2028348c5837381e8
jessicahattig <jessicahattig@gmail.com>
2023-04-12 13:37:15 -0700

    First TDD fail to confirm jest is working.

---
+++ __tests__/haiku.test.js
@@ -0,0 +1,35 @@
+import Haiku from '../src/js/haiku.js';
+
+describe('Haiku', () => {
+  let haiku;
+
+beforeEach(() => {
+  haiku = new Haiku(linesNum);
+});
+});
+
+// test('should create three lines for haiku', () => {
+//   let haiku = lineNum;
+//   expect(haiku.lineNum).toEqual(3);
+//   });
+
+
+
+
+
+// describe('Rectangle', () => {
+//   let rectangle;
+
+//   beforeEach(() => {
+//     rectangle = new Rectangle(3,5);
+//   });
+
+//   test('should correctly create a rectangle object using two sides', () => {
+//     expect(rectangle.side1).toEqual(3);
+//     expect(rectangle.side2).toEqual(5);
+//   });
+
+//   test('should correctly get the area of a rectangle object', () => {
+//expect(rectangle.getArea()).toEqual(15);
+//   });
+// });
